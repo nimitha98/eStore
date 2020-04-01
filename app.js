c318fd76fd4a0a7be5a6aa8badd55a5946498d01
dileepgodithi <dileep.godithi@gmail.com>
2020-04-01 12:51:46 -0500

    authentication added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 var bodyParser = require('body-parser');
 var Product = require('./models/product');
 var Cart = require('./models/cart');
@@ -12,6 +14,24 @@ mongoose.connect('mongodb://localhost:27017/electronics',{useNewUrlParser : true
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 seedDB();
+
+//passport congfiguration
+app.use(require('express-session')({
+    secret : "This is a secret message. Top secret in the world",
+    resave : false,
+    saveUninitialized : false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+})
+
 //index page- show all products
 app.get('/', function(req, res){
     Product.find({}, function(err, products){
@@ -113,6 +133,51 @@ app.post('/cart', function(req, res){
     // })
     res.redirect('/cart');
 });
+
+//AUTH routes
+app.get('/register', function(req, res){
+    res.render('register');
+})
+
+//sign up
+app.post('/register', function(req, res){
+    User.register(new User({username : req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate('local')(req, res, function(){
+            res.redirect('/');
+        });
+    });
+});
+
+//Login routes
+app.get('/login', function(req, res){
+    res.render('login');
+});
+
+//login logic with middleware
+app.post('/login', passport.authenticate('local', {
+    successRedirect : '/',
+    failureRedirect : '/login'
+    }), function(req, res){
+    
+});
+
+//logout
+app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
 
 app.listen(3000, function(){
     console.log('eStore server started and running on port 3000');
